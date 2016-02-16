06c5942d6cc869923f6e0f1b73340abea2150ae8
leppaott <leppanen.otto@gmail.com>
2016-02-16 16:53:52 +0200

    minor

--- Bulletin-Board/src/main/java/BulletinBoard/Database.java
+++ Bulletin-Board/src/main/java/BulletinBoard/Database.java
@@ -48,7 +48,7 @@ public class Database {
     }
 
     public int update(String updateQuery, Object... params) throws SQLException {
-        int changes;
+        int changes = 0;
         try (PreparedStatement stmt = connection.prepareStatement(updateQuery)) {
             for (int i = 0; i < params.length; i++) {
                 stmt.setObject(i + 1, params[i]);
