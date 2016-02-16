56c84cc0837a610b0592667cd2bf5cb36eeceaad
leppaott <leppanen.otto@gmail.com>
2016-02-16 14:55:27 +0200

    project setup

---
+++ Bulletin-Board/src/main/java/BulletinBoard/Main.java
@@ -0,0 +1,19 @@
+package BulletinBoard;
+
+import java.sql.*;
+
+public class Main {
+
+    public static void main(String[] args) throws Exception {
+        Connection connection = DriverManager.getConnection("jdbc:sqlite:tietokanta.db");
+        
+        Statement statement = connection.createStatement();
+
+        ResultSet resultSet = statement.executeQuery("SELECT * FROM ALUE;");
+
+        while (resultSet.next()) {
+            System.out.println(resultSet.getString("nimi"));
+        }
+    }
+
+}
